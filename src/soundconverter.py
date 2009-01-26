bbdfe2d0b7e83fe7671a4469d88c61fa39dfc1ba
kassoulet <kassoulet@4983b5b2-20f1-0310-b9af-a3bc2f16504d>
2009-01-26 21:38:29 +0000

    cosmetics before 1.4.2 release
    git-svn-id: svn+ssh://svn.berlios.de/svnroot/repos/soundconverter/trunk@272 4983b5b2-20f1-0310-b9af-a3bc2f16504d

--- src/soundconverter.py
+++ src/soundconverter.py
@@ -3,7 +3,7 @@
 #
 # SoundConverter - GNOME application for converting between audio formats. 
 # Copyright 2004 Lars Wirzenius
-# Copyright 2005-2008 Gautier Portet
+# Copyright 2005-2009 Gautier Portet
 #
 # This program is free software; you can redistribute it and/or modify
 # it under the terms of the GNU General Public License as published by
