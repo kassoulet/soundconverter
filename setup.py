64b97324c2648ce3597018fdf5f201a0c44b829d
sezanzeb <to.213692@protonmail.ch>
2020-07-19 00:24:52 +0200

    python gitignore and pip installable

---
+++ setup.py
@@ -0,0 +1,11 @@
+from distutils.core import setup
+setup(
+    name='soundconverter',
+    version='3.0.2',
+    description=(
+        'A simple sound converter application for the GNOME environment. '
+        'It writes WAV, FLAC, MP3, and Ogg Vorbis files.'
+    ),
+    license='GPL-3.0',
+    packages=['soundconverter']
+)
