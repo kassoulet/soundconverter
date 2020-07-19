6e01a9ae2d14996302261f8dc4f287ac18ba3b2b
sezanzeb <to.213692@protonmail.ch>
2020-07-19 22:36:51 +0200

    struggle to install po

--- setup.py
+++ setup.py
@@ -1,4 +1,13 @@
-from distutils.core import setup
+import sys
+
+try:
+    import DistUtilsExtra.auto
+except ImportError:
+    sys.stderr.write("To build menulibre you need "
+                     "https://launchpad.net/python-distutils-extra\n")
+    sys.exit(1)
+assert DistUtilsExtra.auto.__version__ >= '2.18', \
+        'needs DistUtilsExtra.auto >= 2.18'
 
 # TODO how is https://github.com/xfce-mirror/catfish installed
 # and https://github.com/bluesabre/menulibre
@@ -6,7 +15,7 @@ from distutils.core import setup
 # TODO figure out how to build a .deb from this
 # how to install the latest source globally? 
 
-setup(
+DistUtilsExtra.auto.setup(
     name='soundconverter',
     version='3.0.2',
     description=(
