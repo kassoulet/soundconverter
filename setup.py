73a3556ed85b10d6f0fc1d6739f95dd00812fd0b
sezanzeb <to.213692@protonmail.ch>
2020-07-20 01:04:19 +0200

    bin and logo to readme

--- setup.py
+++ setup.py
@@ -1,24 +1,12 @@
 import sys
-
-try:
-    import DistUtilsExtra.auto
-except ImportError:
-    sys.stderr.write("To build menulibre you need "
-                     "https://launchpad.net/python-distutils-extra\n")
-    sys.exit(1)
-assert DistUtilsExtra.auto.__version__ >= '2.18', \
-        'needs DistUtilsExtra.auto >= 2.18'
-
-# TODO how is https://github.com/xfce-mirror/catfish installed
-# and https://github.com/bluesabre/menulibre
-
-# TODO figure out how to build a .deb from this
-# how to install the latest source globally? 
+import DistUtilsExtra.auto
 
 # using DistUtilsExtra.auto.setup instead of setup from distutils.core will automatically
-# - install po files to /usr/share/locale*.mo,
+# assuming the prefix is /usr:
+# - compile and install po files to /usr/share/locale*.mo,
 # - install .desktop files to /usr/share/applications
-# - install the py files 
+# - install all the py files to /usr/lib/python3.8/site-packages/soundconverter
+# - copy bin to /usr/bin
 DistUtilsExtra.auto.setup(
     name='soundconverter',
     version='3.0.2',
@@ -27,8 +15,9 @@ DistUtilsExtra.auto.setup(
         'It writes WAV, FLAC, MP3, and Ogg Vorbis files.'
     ),
     license='GPL-3.0',
-    packages=['soundconverter']
     data_files=[
-        ('share/metainfo/', ['data/soundconverter.appdata.xml'])
+        ('share/metainfo/', ['data/soundconverter.appdata.xml']),
+        ('share/pixmaps/', ['data/soundconverter.png']),
+        ('share/icons/hicolor/scalable/apps', ['data/soundconverter.svg']),
     ],
 )
