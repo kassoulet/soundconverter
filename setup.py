803c12b9999a2fa8b91f1a3bfd73c6d06a28c72b
Gautier Portet <gautier.portet@telespazio.com>
2020-10-28 12:48:53 +0100

    Add a message if disutils-extra is missing

--- setup.py
+++ setup.py
@@ -19,6 +19,13 @@
 # Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA 02111-1307
 # USA
 
+import sys
+try:
+    import DistUtilsExtra.auto
+except ImportError:
+    sys.stderr.write('You need python-distutils-extra\n')
+    sys.exit(1)
+
 import os
 import DistUtilsExtra.auto
 
