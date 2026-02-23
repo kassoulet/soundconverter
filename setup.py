4bd4fcc485a12ef2d7f5d4331dd269e0e6f12e65
Gautier <gautier@soundconverter.org>
2026-02-23 11:35:15 +0100

    Set version to 4.1.2

--- setup.py
+++ setup.py
@@ -54,7 +54,7 @@ class Install(DistUtilsExtra.auto.install_auto):
 
 DistUtilsExtra.auto.setup(
     name="soundconverter",
-    version="4.1.1",
+    version="4.1.2",
     description=(
         "A simple sound converter application for the GNOME environment. "
         "It writes WAV, FLAC, MP3, Opus, WMA and Ogg Vorbis files."
